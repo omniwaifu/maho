82198fe72fd48bc4e18738831762f4b66d616dca
Harshit <99947401+Harshit-65@users.noreply.github.com>
2025-06-13 17:10:46 +0530

    fix: python.lang.security.audit.eval-detected.eval-detected-python-helpers-memory.py (#444)
    Co-authored-by: Harshit-65 <kira@offgridsec.com>

--- src/helpers/memory.py
+++ src/helpers/memory.py
@@ -1,3 +1,4 @@
+import ast
 from datetime import datetime
 from typing import Any, List, Sequence
 from langchain.storage import InMemoryByteStore, LocalFileStore
@@ -394,7 +395,7 @@ class Memory:
     def _get_comparator(condition: str):
         def comparator(data: dict[str, Any]):
             try:
-                return eval(condition, {}, data)
+                return ast.literal_eval(condition, {}, data)
             except Exception as e:
                 # PrintStyle.error(f"Error evaluating condition: {e}")
                 return False
